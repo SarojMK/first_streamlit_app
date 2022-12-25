bdd6da92419e024c600db5dbf50feddcf14597cc
Saroj Kumar Moharana <121400080+SarojMK@users.noreply.github.com>
2022-12-25 11:28:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header(BreakFast Menu')
+streamlit.header('BreakFast Menu')
 
-streamlit.text(Idly, Sambar')
-streamlit.text(Dosa, Chutny')
+streamlit.text('Idly, Sambar')
+streamlit.text('Dosa, Chutny')
