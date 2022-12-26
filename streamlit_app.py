2c78615d55f1b7ef123daf903a900a0fc55e198c
Saroj Kumar Moharana <121400080+SarojMK@users.noreply.github.com>
2022-12-26 13:17:40 -0700

    Update streamlit_app.py
    Correcting position of commands

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,12 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-
-import requests
 # Adding Text input box
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # Normalize the json version of the response
